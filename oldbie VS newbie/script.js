3f452cd60662bdfe8f3e2672214cd66b57a28764
jellyjoji <74365275+jellyjoji@users.noreply.github.com>
2022-05-10 00:58:38 +0900

    with memo

--- oldbie VS newbie/script.js
+++ oldbie VS newbie/script.js
@@ -3,10 +3,11 @@ const titleBtn = document.querySelector("#startBtn");
 const questionContainer = document.querySelector('.question-container');
 const question = document.querySelector('#question_title');
 const type = document.querySelector('#question_content');
+// answer button 
 const aBtn = document.querySelector("#a");
 const bBtn = document.querySelector('#b');
 
-
+// this is answer button
 const oldbie = document.querySelector('#oldbie');
 const newbie = document.querySelector('#newbie');
 
@@ -17,16 +18,20 @@ const explain = document.querySelector('#explain');
 const image = document.querySelector('#result-img');
 const resultContainer = document.querySelector('.result-container');
 
+// this is question
 const q = {
     1: {
+        // title
         "title": "문제 1번",
+        // put contnents
         "content": "뒷푸영을 가봤다",
+        // answer button copywrite
         "A": "네",
         "B": "아니요"
-        // o : old / n : new
+        // A : oldbie / B : newbie
     },
     2: { "title": "문제 2번", 
-    "content": "대체번호 입력을 통해 출결을 해봤다", 
+    "content": "대체번호 입력을 통해 출결을 해봤다",
     "A": "네",
     "B": "아니요"},
     3: { "title": "문제 3번", 
@@ -86,31 +91,37 @@ const q = {
     "A": "아니요",
     "B": "네"}
 }
+// this is result
 const result = {
     "first_grade": {
         "person": "아무것도 모르는 순수한 새내기",
         "explain": "캠퍼스라이프를 막 시작하면서 캠퍼스의 풍경들, 사람들, 교수님들, 강의실, 축제, 동아리 모든 게 신기해요! 영남대는 왜 이렇게 큰지 길을 헤매다가 지도를 보고 강의실에 겨우 도착하기도 했구요. 입고싶은 옷도 마음껏 입고, 술도 마음껏 마실거예요!",
+        // insert img here
         "img": "image/first_grade.png"
     },
-    "second_grade": { "person": "조금씩 적응해가는 영대인",
-     "explain": "대학교에 들어와서 캠퍼스 라이프에 점점 적응해가고 있어요. 이젠 눈 감고도 강의실을 찾아갈 수 있을 것 같아요! 캠퍼스에 대한 환상도 점점 걷혀가지만 우리에겐 종강이 있으니까요.",
-      "img": "image/second_grade.jpg" },
-    "third_grade": { "person": "아무것도 모르는 뉴비가 귀여운 고학년 학교 시설을 통달하신 당신",
-     "explain": "분명 영남대가 되게 크게 느껴졌는데 지금은 그렇게 큰 것 같지도 않아요. 내가 영남대에서 필요한 것이 어디에 있는지 다 파악하고 있으니까요. 오늘도 옷은 최대한 편하게 입고, 모자도 푹 눌러쓰고 학교를 가요. 혹시 과잠에 대한 미련도 점점 사라져가고 있진 않으신가요?",
-      "img": "image/third_grade.png"},
-    "fourth_grade": { "person": "모든 것을 통달한 올드비",
-     "explain": "나는 가만히 학교다닌 죄밖에 없는데 사람들이 나를 화석이라 부르기 시작해서 속상해요. 학년만 높지 아직 어린데 동아리 모집에 저는 부르지 않아서 속상해요.",
-      "img": "image/fourth_grade.png" },
-    "fifth_grade": { "person": "캠퍼스의 역사를 경험한 화석",
-     "explain": "영대에 많은 것이 사라지고 생기는 것을 두 눈으로 본 당신. 혹시 학번을 말했을 때 주변사람들이 다들 놀라지 않던가요?",
-      "img": "image/fifth_grade.jpg" }
+    "second_grade": { 
+        "person": "조금씩 적응해가는 영대인",
+        "explain": "대학교에 들어와서 캠퍼스 라이프에 점점 적응해가고 있어요. 이젠 눈 감고도 강의실을 찾아갈 수 있을 것 같아요! 캠퍼스에 대한 환상도 점점 걷혀가지만 우리에겐 종강이 있으니까요.",
+        "img": "image/second_grade.jpg" },
+    "third_grade": { 
+        "person": "아무것도 모르는 뉴비가 귀여운 고학년 학교 시설을 통달하신 당신",
+        "explain": "분명 영남대가 되게 크게 느껴졌는데 지금은 그렇게 큰 것 같지도 않아요. 내가 영남대에서 필요한 것이 어디에 있는지 다 파악하고 있으니까요. 오늘도 옷은 최대한 편하게 입고, 모자도 푹 눌러쓰고 학교를 가요. 혹시 과잠에 대한 미련도 점점 사라져가고 있진 않으신가요?",
+        "img": "image/third_grade.png"},
+    "fourth_grade": { 
+        "person": "모든 것을 통달한 올드비",
+        "explain": "나는 가만히 학교다닌 죄밖에 없는데 사람들이 나를 화석이라 부르기 시작해서 속상해요. 학년만 높지 아직 어린데 동아리 모집에 저는 부르지 않아서 속상해요.",
+        "img": "image/fourth_grade.png" },
+    "fifth_grade": { 
+        "person": "캠퍼스의 역사를 경험한 화석",
+        "explain": "영대에 많은 것이 사라지고 생기는 것을 두 눈으로 본 당신. 혹시 학번을 말했을 때 주변사람들이 다들 놀라지 않던가요?",
+        "img": "image/fifth_grade.jpg" }
 }
 
 
 let num = 1;
 
-// 여기까지만 사전 제공
-
+// click 하면 동작하게 한다
+// 첫화면 버튼을 누르면 커버가 없어지고 문제가 나오도록 해라
 titleBtn.addEventListener('click', () => {
     titleContainer.style.display = 'none';
     questionContainer.style.display = 'block';
@@ -119,12 +130,17 @@ titleBtn.addEventListener('click', () => {
 
 aBtn.addEventListener('click', () => {
     switch (type.innerHTML) {
+        // 첫번째 올드비 유형 
         case 'oldbie':
+            // parseInt : 글자를 숫자로 전환
             let o = parseInt(oldbie.value);
+            // 네, 올드비를 선택하면 올드비 숫자창에 +1 씩 더해준다. 
             oldbie.setAttribute('value', o + 1);
             break;
+        // 두번째 뉴비 유형
         case 'newbie':
             let n = parseInt(newbie.value);
+            // 네, 뉴비를 선택하면 올드비 숫자창에 +1 씩 더해준다. 
             newbie.setAttribute('value', n + 1);
             break;
     }
@@ -144,23 +160,32 @@ bBtn.addEventListener('click', () => {
     }
     updateQuestion();
 });
-
+// 결과 도출 : 1학년 ~ 5학년 
+// 문제의 갯수는 16개니까 4점 1학년, 7점 2학년, 10점 3학년, 13점 4학년, 16 5학년 으로 나누도록 한다
 function updateQuestion() { 
     if (num == 16) {
+        // 16번 문제가 되었을때 문제는 더이상 보이지 않도록 하고 결과창을 보여주겠다 
         questionContainer.style.display = 'none';
         resultContainer.style.display = 'block';
 
+        // 만약 올드비의 결과값이 4이상이면 첫번째 결과를 불러오겠다
         if(oldbie.value<=4){ result_grade = "first_grade"}
+        // 아니면 올드비의 결과값이 4이상 7이하이면 두번째 결과를 불러오겠다
         else if(4<oldbie.value<=7){ result_grade = "second_grade"}
         else if(7<oldbie.value<=10){ result_grade = "third_grade"}
         else if(10<oldbie.value<=13){ result_grade = "fourth_grade"}
+        // 모두 다 아니면 다섯번째 결과를 불러오겠다
         else{result_grade = "fifth_grade"}
         
+        // 사람에 대한 결과값을 가져오겠다
         grade.innerHTML = result[result_grade].person;
+        // 설명 대한 결과값을 가져오겠다
         explain.innerHTML = result[result_grade].explain;
         image.setAttribute('src', result[result_grade].img)
     }
     else {
+        // pro : progress bar
+        // progress bar 의 길이 css 를 js 에서 조정해준다
         pro.setAttribute('style', `width : calc(100/16*${num}%`);
         question.innerHTML = q[num].title;
         type.innerHTML = q[num].content;
